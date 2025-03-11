e97dfe6b9997813a92d181052746226c988df2ee
Aldrin Caalim <aldrinmcaalim@gmail.com>
2025-03-11 17:18:23 -0400

    ui design finished

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { HomeScreen } from "./screens/feature-specific/Home.screen.jsx";
+import { LoginScreen } from "./screens/auth/Login.screen.jsx";
+import { SignUpScreen } from "./screens/auth/SignUp.screen.jsx";
+import { NotificationScreen } from "./screens/feature-specific/Notification.screen.jsx";
+import { ProfileScreen } from "./screens/feature-specific/Profile.screen.jsx";
+import { Route, Routes } from "react-router-dom";
+import { Sidebar } from "./components/common/Sidebar.component.jsx";
+import { RightPanel } from "./components/common/RightPanel.component.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex max-w-6xl mx-auto">
+      {/* common component since it isn't wrapped in routes */}
+      <Sidebar />
+      <Routes>
+        <Route index={true} path="/" element={<HomeScreen />} />
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/signup" element={<SignUpScreen />} />
+        <Route path="/notifications" element={<NotificationScreen />} />
+        <Route path="/profile/:username" element={<ProfileScreen />} />
+      </Routes>
+      <RightPanel />
+    </div>
+  );
 }
 
-export default App
+export default App;
